ec2800135534dc0c00380d280b1b80399329be4c
Md. Emran Hossen <emranhossen3075@gmail.com>
2024-05-07 02:55:36 +0600

    Update Logic

--- src/Assignment1/Task1/Program.cs
+++ src/Assignment1/Task1/Program.cs
@@ -1,22 +1,33 @@
 ﻿int n = int.Parse(Console.ReadLine());
+
 int[,] arr = new int[n, n];
+
 for (int i = 0; i < n; i++)
 {
-    string[] input = Console.ReadLine().Split();
+    string[] input = Console.ReadLine().Split(" ");
     for (int j = 0; j < n; j++)
     {
         arr[i, j] = int.Parse(input[j]);
+       
     }
 }
 int sum1 = 0;
 int sum2 = 0;
+int l = n - 1;
 for (int i = 0; i < n; i++)
-{
+{   
     for (int j = 0; j < n; j++)
     {
-        sum1 += arr[i, i];
-        sum2 += arr[i, n - 1 - i];
+       if (i == j)
+        {
+            sum1 += arr[i, j];
+        }
+       if (j == l)
+        {
+            sum2 += arr[i, j];
+        }
     }
+    l--;
 }
 Console.WriteLine($"1st Diagonal Sum: {sum1}");
 Console.WriteLine($"2nd Diagonal Sum: {sum2}");
