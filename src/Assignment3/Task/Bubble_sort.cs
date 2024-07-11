17d3f59a78b522783f68f88762a392a694942575
Md. Emran Hossen <emranhossen3075@gmail.com>
2024-07-11 15:41:10 +0600

    Complete Assignment3

---
+++ src/Assignment3/Task/Bubble_sort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    internal class Bubble_sort<T>
+      where T : IComparable
+    {
+        public void Sort(T[] numbers)
+        {
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                bool flag = false;
+                for (int j = 0; j < numbers.Length - 1 - i; j++)
+                {
+                    if (numbers[j].CompareTo(numbers[j + 1]) > 0)
+                    {
+                        T temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                        flag = true;
+                    }
+                }
+                if (flag == false)
+                {
+                    break;
+                }
+            }
+            foreach (T i in numbers)
+            {
+                Console.Write(i);
+                Console.Write(' ');
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- src/Assignment3/Task/Program.cs
+++ src/Assignment3/Task/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Task;
+
+Bubble_sort<int> bubble = new Bubble_sort<int>();
+int[] intArray = { 34, 7, 5, 3, 7, 9 };
+bubble.Sort(intArray);
+
+Bubble_sort<double> bubble1 = new Bubble_sort<double>();
+double[] doubleArray = { 3.4, 7.1, 2.3, 3.2, 5.6, 6.2 };
+bubble1.Sort(doubleArray);
+
+Bubble_sort<string> bubble2 = new Bubble_sort<string>();
+string[] stringArray = { "Emran", "Rakib", "Shakib", "sajib","zakir"};
+bubble2.Sort(stringArray);
+
+
+
+
